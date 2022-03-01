30400e44d04257834c577ec94927d1912978f3d5
Cumaribo <38987055+Cumaribo@users.noreply.github.com>
2022-03-01 18:31:50 -0500

    Update Palsar_preprocessingV0.2.R
    Improvements with environment and data management

--- Palsar_preprocessingV0.2.R
+++ Palsar_preprocessingV0.2.R
@@ -3,15 +3,19 @@
 # Authors: Jeronimo Rodriguez and Victoria Sarmiento 2020
 # Update: March 2022
 
-
-
-
                                         # Required libraries --------------
 dir()
 
 packs <- c('raster', 'tidyverse', 'furrr')
 sapply(packs, require, character.only = TRUE)
 
+# stew tempdirectory in currenbt wd
+dir.create("tempfiledir")
+tempdir=paste(getwd(),"tempfiledir", sep="//")
+rasterOptions(tmpdir=tempdir)
+# 
+
+# create untar function
 files <- list.files(".", "tar.gz")
 # Create function for untar
 fdc <- function(files,i){
@@ -27,57 +31,49 @@ for(i in 1:length(files)){
     tiffes[[i]] <- fdc(files,i)
 }
 
-
 # CREATE A LIST OF HH/HV FILES ---------------------------------------------
 # You need to create a list of the actual raster. The code below selects ".HV_F02DAR" for the HV band
 # or ".HH_F02DAR" for the HH band. Take into account that the pattern in the name could change depending on the
 # date of your Palsar Data.
 # IMPORTANT: You need to include "$" at the end so R will not include the ".hdr" files in the list.
 # ** You need to perform this step for each band (HH/HV).
+                                        #List HH/HV files
+#This is missing. Depending on the year, the names of the rasters change slightly. (include the "F02DAR" or not)
+# and needs to be adjusted manually
+files1 <- list.files(".", "sl_HH_F02DAR$")
+files2 <- list.files(".", "sl_HV_F02DAR$")
 
-                                        #List only HH/HV files
-files1 <- list.files(".", "sl_HH$")
-files2 <- list.files(".", "sl_HV$")
+mem_future <- 10000*1024^2 #this is to set the limit to 1GB
+options(future.globals.maxSize= mem <- future)
+plan(multisession, workers=2)
                                         # create an empty list
 r.list1 <- list()
 r.list2 <- list()                                        # load the rasters
 for(i in 1:length(files1)){
     r.list1[[i]] <- raster(files1[i])}
 for(i in 1:length(files2)){
-  r.list2[[i]] <- raster(files2[i])}
-# merge the raster/create the mosaic
-
-yesr<-set manually
+ # merge the raster/create the mosaic
+set year
+  year<-'2018'
 namer<-c('HH', 'HV')
-dater<-### fill this ####
+#dater<-### fill this ####
 merged1 <- do.call(merge, r.list1)
 merged2 <- do.call(merge, r.list2)
 merged<-list(merged1,merged2)
-merged2 <- merge(r.list[[1]], merged)
-msk<-raster('/mask_.tif') 
-merged <- future_map(1:length(merged), function(x) crop(merged[[x]], extent(msk)))
+msk<-raster('/path/to/template/template.tif') 
+merged <- map(1:length(merged), function(x) crop(merged[[x]], extent(msk)))
 future_map(1:length(merged), function(x) writeRaster(merged[[x]], paste(namer[x], year, sep='_'), format='GTiff', 
                                                                         overwrite=TRUE)) 
-# here I restar after the Lee filter
-# 
+# load the filtered rasters  
 
-
-setwd("/Lee_filter")
-
-mem_future <- 10000*1024^2 #this is to set the limit to 1GB
-options(future.globals.maxSize= mem <- future)
-plan(multisession, workers=2)
-
+setwd("path/to/thelee outputs ")
 files <- list.files(".", "tif")
 r.list <- list()
 for(i in 1:length(files)){
     r.list[[i]] <- raster(files[i])}
-
+# sigma naught correction function
 sigma_n <- function(pals){
     filtered <- 10*log10(pals^2)-83
 }
-
 r.list <- map(1:length(r.list), function(x) sigma_n(r.list[[x]])) 
-
 map(1:length(r.list), function(x) writeRaster(r.list[[x]],paste(names(r.list[[x]]), 'SN', sep='_'),format='GTiff'))
-
